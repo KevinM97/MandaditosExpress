1d0c044ecc0ce8af7ebadfd9018985fc1f791648
Kevin Mina <58663814+KevinM97@users.noreply.github.com>
2021-11-28 20:31:06 -0500

    Primer Comit de la aplicación

---
+++ android/app/src/main/kotlin/com/example/mandaditosexpress/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mandaditosexpress
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Inicio")),
+      body: const Center(child: Text("Inicio")),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Usuario',
+            backgroundColor: Colors.orange,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shop),
+            label: 'Mandado',
+            backgroundColor: Colors.orange,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: 'Chat',
+            backgroundColor: Colors.orange,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble_outline_rounded),
+            label: 'Opinión',
+            backgroundColor: Colors.orange,
+          ),
+        ],
+        currentIndex: 0,
+      ),
+    );
+  }
+}
