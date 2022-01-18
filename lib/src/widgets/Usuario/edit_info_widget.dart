ef5f7aab31287300bcc877637fa2ae00611cd79a
KevinM97 <khmina@espe.edu.ec>
2022-01-18 11:50:53 -0500

    Merge branch 'Master' of https://github.com/KevinM97/MandaditosExpress into Master

--- lib/src/widgets/Usuario/edit_info_widget.dart
+++ lib/src/widgets/Usuario/edit_info_widget.dart
@@ -1,24 +1,10 @@
-<<<<<<< HEAD
-=======
 import 'package:cloud_firestore/cloud_firestore.dart';
->>>>>>> db8ef9828f118bd082695b3713649df9f11208e2
 import 'package:flutter/material.dart';
 
 class EditInfoWidget extends StatefulWidget {
   const EditInfoWidget({Key? key}) : super(key: key);
 
   @override
-<<<<<<< HEAD
-  _EditInfoWidget createState() => _EditInfoWidget();
-}
-
-class _EditInfoWidget extends State<EditInfoWidget> {
-  late TextEditingController textController1;
-  late TextEditingController textController2;
-  late TextEditingController textController3;
-  final formKey = GlobalKey<FormState>();
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-=======
   _EditInfoWidgetState createState() => _EditInfoWidgetState();
 }
 
@@ -30,7 +16,6 @@ class _EditInfoWidgetState extends State<EditInfoWidget> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   final Stream<QuerySnapshot> _editStream =
   FirebaseFirestore.instance.collection('Cliente').snapshots();
->>>>>>> db8ef9828f118bd082695b3713649df9f11208e2
 
   @override
   void initState() {
@@ -46,7 +31,6 @@ class _EditInfoWidgetState extends State<EditInfoWidget> {
       key: formKey,
       child: Scaffold(
         key: scaffoldKey,
-<<<<<<< HEAD
         appBar: AppBar(
           backgroundColor: Colors.deepOrange,
           automaticallyImplyLeading: true,
@@ -242,179 +226,9 @@ class _EditInfoWidgetState extends State<EditInfoWidget> {
                 ),
               ),
             ],
-=======
-        backgroundColor: Colors.white,
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
-            child: ListView(
-              padding: EdgeInsets.zero,
-              scrollDirection: Axis.vertical,
-              children: [
-                Container(
-                  width: 100,
-                  height: 100,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                  ),
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    TextFormField(
-                      controller: textController1,
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        hintText: 'Nombre de Usuario',
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Colors.black,
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Colors.black,
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        contentPadding:
-                            const EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
-                        prefixIcon: const Icon(
-                          Icons.person,
-                        ),
-                      ),
-                      textAlign: TextAlign.start,
-                      validator: (val) {
-                        if (val!.isEmpty) {
-                          return 'Field is required';
-                        }
-
-                        return null;
-                      },
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: const [
-                    Divider(
-                      height: 30,
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    TextFormField(
-                      controller: textController2,
-                      obscureText: false,
-                      decoration: InputDecoration(
-                        hintText: 'Nombre y apellido',
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Colors.black,
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Colors.black,
-                            width: 1,
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        contentPadding:
-                            const EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
-                        prefixIcon: const Icon(
-                          Icons.location_history_sharp,
-                        ),
-                      ),
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.max,
-                      children: const [
-                        Divider(
-                          height: 30,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        TextFormField(
-                          controller: textController3,
-                          obscureText: false,
-                          decoration: InputDecoration(
-                            hintText: 'Correo electrónico',
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                color: Colors.black,
-                                width: 1,
-                              ),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: const BorderSide(
-                                color: Colors.black,
-                                width: 1,
-                              ),
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            contentPadding:
-                                const EdgeInsetsDirectional.fromSTEB(10, 0, 0, 10),
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        const Divider(
-                          height: 150,
-                        ),
-                        Column(
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            Center(
-                              child: _renderButton(
-                                text: 'Confirmar Cambios',
-                                color: Colors.blue,
-                                onTap: () {},
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
-            ),
->>>>>>> db8ef9828f118bd082695b3713649df9f11208e2
           ),
         ),
       ),
     );
   }
-<<<<<<< HEAD
-=======
-
-  Widget _renderButton({
-    required String text,
-    required Color color,
-    required Function() onTap,
-  }) {
-    return ListTile(
-      
-      onTap: onTap,
-      tileColor: Colors.deepOrange,
-      title: Text(text),
-    );
-  }
->>>>>>> db8ef9828f118bd082695b3713649df9f11208e2
 }
