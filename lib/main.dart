ac27d8e9d4b1e16d3a05bd20febaf1a700f5d3c2
KevinM97 <khmina@espe.edu.ec>
2022-01-18 01:32:18 -0500

    Se arregló los cambios de integración de firebase con la app

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:mandaditosexpress/src/pages/home_page.dart';
 import 'package:mandaditosexpress/src/theme/main_theme.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'providers/theme_provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     MultiProvider(
       providers: [
